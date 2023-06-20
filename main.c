311ac3a9ea8b443d639749912c297c65c34ab6c1
Neylz <56648332+Neylz@users.noreply.github.com>
2023-06-20 20:02:50 +0200

    send messages to server

--- main.c
+++ main.c
@@ -1,65 +1,61 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-#include <arpa/inet.h>
-#include <winsock.h>
-
-#define SERVER_IP "192.168.1.13"  // Replace with the server IP address
-#define SERVER_PORT 12345      // Replace with the server port number
-
-void send_request(int client_socket, const char* request) {
-    // Send the request to the server
-    write(client_socket, request, strlen(request));
-}
-
-void receive_response(int client_socket) {
-    char response[1024];
-    memset(response, 0, sizeof(response));
-
-    // Receive the response from the server
-    read(client_socket, response, sizeof(response));
-
-    // Process the response (e.g., perform actions based on the response)
-    printf("Received response from server: %s\n", response);
-}
-
 int main() {
-    // Create a socket for the client
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Socket creation failed");
+
+    printf("Client started.\n");
+
+    const char *hostName = "192.168.137.1";
+    const char *portNumber = "8808";
+    int clientSocket;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    struct addrinfo *results;
+    struct addrinfo *record;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    if ((getaddrinfo(hostName, portNumber, &hints, &results)) != 0) {
+        perror("Failed to translate client socket.");
         exit(EXIT_FAILURE);
     }
 
-    // Define the server address and port
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(SERVER_PORT);
+    printf("Client socket translated.\n");
 
-    // Convert IP address from text to binary format
-    if (inet_pton(AF_INET, SERVER_IP, &(server_address.sin_addr)) <= 0) {
-        perror("Invalid address or address not supported");
+    for (record = results; record != NULL; record = record->ai_next) {
+        clientSocket = socket(record->ai_family, record->ai_socktype, record->ai_protocol);
+        if (clientSocket == -1) continue;
+        if (connect(clientSocket, record->ai_addr, record->ai_addrlen) != -1) break;
+        close(clientSocket);
+    }
+
+    if (record == NULL) {
+        perror("Failed to create or connect client socket.");
         exit(EXIT_FAILURE);
     }
 
-    // Connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-        perror("Connection failed");
+    freeaddrinfo(results);
+
+    printf("Client socket created and connected.\n");
+
+    char *message = "test message from client";
+
+    if (send(clientSocket, message, strlen(message), 0) == -1) {
+        perror("Failed to perform cognitive recalibration.");
         exit(EXIT_FAILURE);
+    } else {
+        printf("Message sent to server.\n");
     }
 
-    // Send the data to the server
-    const char* data = "Data to be sent to the server";
-    send_request(client_socket, data);
+    close(clientSocket);
 
-    // Receive and process the server response
-    receive_response(client_socket);
-
-    // Close the socket
-    close(client_socket);
+    printf("Client socket closed.\n");
 
     return 0;
 }
